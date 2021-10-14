db8cc95dd5ddf86310a75a8c6a9f98c648f985f2
Vadim Hryn <gryn.vadim@gmail.com>
2021-10-13 20:31:35 -0400

    added a list of movies and some links

--- index.js
+++ index.js
@@ -4,18 +4,31 @@ morgan = require('morgan');
 const app = express();
 let topMovies =
 [
-  { title: 'Pig', director: 'Michael Sarnoski', releaseDate: 'July 16, 2021' },
-  { title: 'Zola', director: 'Janicza Bravo (Lemon)', releaseDate: 'June 30, 2021' },
-  { title: 'Annette', director: 'Leos Carax (Holy Motors)', releaseDate: 'August 6, 2021' },
-  { title: 'Saint Maud', director: 'Rose Glass', releaseDate: 'January 29, 2021' },
-  { title: 'Barb & Star Go to Vista Del Mar', director: 'Josh Greenbaum', releaseDate: 'February 12, 2021' },
-  { title: 'Test Pattern', director: 'Shatara Michelle Ford', releaseDate: 'February 12, 2021' },
-  { title: 'The Green Knight', director: ' David Lowery (A Ghost Story)', releaseDate: 'July 30, 2021' },
-  { title: 'The Card Counter', director: 'Paul Schrader (First Reformed)', releaseDate: 'September 10, 2021' },
-  { title: 'Shiva Baby', director: 'Emma Seligman', releaseDate: 'April 3, 2021' },
-  { title: 'Bad Trip', director: 'Kitao Sakurai', releaseDate: 'March 26, 2021' }
+  { title: 'The Green Mile', director: 'Frank Darabont', releaseDate: '1999', genres: '' },
+  { title: 'The Shawshank Redemption', director: 'Frank Darabont', releaseDate: '1994', genres: '' },
+  { title: 'The Lord of the Rings: The Return of the King', director: 'Peter Jackson', releaseDate: '2003', genres: '' },
+  { title: 'The Lord of the Rings: The Two Towers', director: 'Peter Jackson', releaseDate: '2002', genres: '' },
+  { title: 'The Lord of the Rings: The Fellowship of the Ring', director: 'Peter Jackson', releaseDate: '2001', genres: '' },
+  { title: 'Forrest Gump', director: 'Robert Zemeckis', releaseDate: '1994', genres: '' },
+  { title: 'Lock, Stock and Two Smoking Barrels', director: ' Guy Ritchie', releaseDate: '1998', genres: '' },
+  { title: 'The Matrix', director: 'Lana Wachowski', releaseDate: '1999', genres: '' },
+  { title: 'Inception', director: 'Christopher Nolan', releaseDate: '2010', genres: '' },
+  { title: 'Interstellar', director: 'Christopher Nolan', releaseDate: '2014', genres: '' }
 ];
-
+let directors = [
+  {name: 'Frank Darabont', dateOfBirth: 'January 28, 1959'},
+  {name: 'Peter Jackson', dateOfBirth: 'Oktober 31, 1961'},
+  {name: 'Robert Zemeckis', dateOfBirth: 'May 14, 1951 '},
+  {name: 'Guy Ritchie', dateOfBirth: 'September 10, 1968 '},
+  {name: 'Lana Wachowski', dateOfBirth: 'June 21, 1965 '},
+  {name: 'Christopher Nolan', dateOfBirth: 'July 30, 1970 '}
+];
+let users = [
+  {name: 'Vadym',
+   email: 'example@gmail.com',
+   favorites: ['The Lord of the Rings: The Return of the King' , 'Inception']
+ },
+];
 app.use(express.static('public'));
 
 app.use(morgan('common'));
@@ -26,6 +39,32 @@ app.get('/', (req, res) => {
 });
  app.get('/movies', (req, res) => {
    res.json(topMovies);
+ });
+ app.get('/movies/:title', (req, res) => {
+   res.json(topMovies.find((movies) =>
+     { return movies.title === req.params.title }));
+ });
+ app.get('/genres/:title', (req, res) => {
+   res.send('Successful GET request returning data about a genre by name/title');
+ });
+ app.get('/directors/:name', (req, res) => {
+   res.json(directors.find((director) =>
+     { return director.name === req.params.name }));
+ });
+ app.post('/users', (req, res) => {
+   res.send('Successful POST request allowing new users to register');
+ });
+ app.post('/users/:id/favoriteMovies/:name', (req, res) => {
+   res.send('Successful POST request allowing users to add a movie to their list of favorites');
+ });
+ app.put('/users/:name', (req, res) => {
+   res.send('Successful PUT request allowing users to update their user info');
+ });
+ app.delete('/users/:id/favoriteMovies/:name', (req, res) => {
+   res.send('Successful DELETE request allowing users to remove a movie from their list of favorites');
+ });
+ app.delete('/users/:id', (req, res) => {
+   res.send('Successful DELETE request allowing existing users to deregister');
  });
  // listen for requests
  app.listen(8080, () => {
